41e259b0444abbff54c3581ec9abed88647cf1b4
codeAreAll <812683139@qq.com>
2022-02-03 16:09:44 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+
+
+
+
+
+
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
 </script>
 
 <style lang="less">
@@ -20,6 +26,11 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+
+
+
+
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
